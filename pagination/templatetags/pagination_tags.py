db4477bc125bcceb8f722787b420af01ce1f68c2
Thomas Orozco <thomas@orozco.fr>
2015-09-16 21:32:24 +0200

    Fix import for Django 1.8

--- pagination/templatetags/pagination_tags.py
+++ pagination/templatetags/pagination_tags.py
@@ -4,7 +4,7 @@ except NameError:
     from sets import Set as set
 
 from django import template
-from django.template import TOKEN_BLOCK
+from django.template.base import TOKEN_BLOCK
 from django.http import Http404
 from django.core.paginator import InvalidPage
 from django.conf import settings
