b16558ebcde6bb62ab06426311373bddd68fb98b
Tomas Zulberti <tomas@jampp.com>
2020-05-09 21:03:09 -0300

    Se actualiza para que use el Mixin de django de ser posible

--- pagination/middleware.py
+++ pagination/middleware.py
@@ -1,16 +1,19 @@
-class MiddlewareMixin:
-    def __init__(self, get_response=None):
-        self.get_response = get_response
-        super().__init__()
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    class MiddlewareMixin:
+        def __init__(self, get_response=None):
+            self.get_response = get_response
+            super().__init__()
 
-    def __call__(self, request):
-        response = None
-        if hasattr(self, 'process_request'):
-            response = self.process_request(request)
-        response = response or self.get_response(request)
-        if hasattr(self, 'process_response'):
-            response = self.process_response(request, response)
-        return response
+        def __call__(self, request):
+            response = None
+            if hasattr(self, 'process_request'):
+                response = self.process_request(request)
+            response = response or self.get_response(request)
+            if hasattr(self, 'process_response'):
+                response = self.process_response(request, response)
+            return response
 
 
 def get_page(self):
